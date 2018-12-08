2b698d4056f3362c8b718741f903227230691f52
SongChunPeng <154518499@qq.com>
2018-12-08 15:40:59 +0800

    自定义Ribbon负载均衡策略

--- consumer-user/src/main/java/com/toprs/consumeruser/ConsumerUserApplication.java
+++ consumer-user/src/main/java/com/toprs/consumeruser/ConsumerUserApplication.java
@@ -18,7 +18,7 @@ import org.springframework.web.client.RestTemplate;
  * 将Ribbon配置类放到SpringBoot启动类扫描范围之外.
  * 如果放到启动类扫描范围之内,自定义@ExcludeFromComponentScan注解,
  * 添加到@ComponentScan的excludeFilters中.
- * 标注@ExcludeFromComponentScan注解将排除到SpringBoot启动类的扫描范围内
+ * 标注@ExcludeFromComponentScan注解的类将排除到SpringBoot启动类的扫描范围
  */
 @RibbonClient(name = "provider-user", configuration = RibbonConfiguration.class)
 @ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.ANNOTATION, value = ExcludeFromComponentScan.class) })

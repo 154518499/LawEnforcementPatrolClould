834a72ea5f4890b80e5f496657dc46250a539f01
SongChunPeng <154518499@qq.com>
2018-12-08 16:30:53 +0800

    Ribbon脱离Eureka使用

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LawEnforcementPatrolClould
 8761 eureka_one  
 1110 provider-user  
-1110 provider-user-one  
+1111 provider-user-one  
 8081 sso  
 6661 consumer-user
---
+++ ribbon-without-eureka/src/main/java/com/example/ribbonwithouteureka/RibbonWithoutEurekaApplication.java
@@ -0,0 +1,20 @@
+package com.example.ribbonwithouteureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class RibbonWithoutEurekaApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonWithoutEurekaApplication.class, args);
+	}
+}

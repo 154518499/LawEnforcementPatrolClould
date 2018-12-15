363f6f7fa30392263e203aba969f8a28e956af7e
SongChunPeng <154518499@qq.com>
2018-12-15 10:10:24 +0800

    eureka 高可用集群

---
+++ eureka-high-availability/src/test/java/com/toprs/eurekahighavailability/EurekaHighAvailabilityApplicationTests.java
@@ -0,0 +1,17 @@
+package com.toprs.eurekahighavailability;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaHighAvailabilityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
